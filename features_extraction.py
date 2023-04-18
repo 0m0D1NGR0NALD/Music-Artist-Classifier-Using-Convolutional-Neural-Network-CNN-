87a98eff1f30f9810bfdd0cad5fa4aa965b95192
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-18 15:22:05 +0300

    Update features_extraction.py

--- features_extraction.py
+++ features_extraction.py
@@ -11,3 +11,4 @@ def get_mfcc(wav_file_path):
 def get_melspectrogram(wav_file_path):
   y, sr = librosa.load(wav_file_path, offset=0, duration=30)
   melspectrogram = numpy.array(librosa.feature.melspectrogram(y=y, sr=sr))
+  return melspectrogram
