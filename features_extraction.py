074e5bfca55ecfc97503f01e3917e67e56c5389a
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-19 08:24:29 +0300

    Update features_extraction.py

--- features_extraction.py
+++ features_extraction.py
@@ -1,7 +1,7 @@
 import librosa
 import numpy
 
-# Mel Frequency Cepstrl Coefficients (MFCC)
+# Mel Frequency Cepstral Coefficients (MFCC)
 def get_mfcc(wav_file_path):
   y, sr = librosa.load(wav_file_path, offset=0, duration=30)
   mfcc = numpy.array(librosa.feature.mfcc(y=y, sr=sr))
