db74d4dcc92e3295480bbd2f8171263f8dbe1655
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-19 08:29:46 +0300

    Update display_features.py

--- display_features.py
+++ display_features.py
@@ -2,3 +2,6 @@ from matplotlib import pyplot
 import numpy
 import librosa
 from extract_features import get_mfcc,get_melspectrogram,get_chroma_vector,get_tonnetz
+
+# Sample audio path
+audio = 'juliana_2.wav'
