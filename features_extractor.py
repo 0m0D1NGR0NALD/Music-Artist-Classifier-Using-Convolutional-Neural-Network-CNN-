a79a7e64a30553da652e234c8794aa96f564d373
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-22 09:15:50 +0300

    Update and rename feature_extractor.py to features_extractor.py

--- features_extractor.py
+++ features_extractor.py
@@ -1,5 +1,5 @@
 import numpy
-from feature_extraction_fxns import get_mfcc,get_melspectrogram,get_chroma_vector,get_tonnetz
+from features_extraction_fxns import get_mfcc,get_melspectrogram,get_chroma_vector,get_tonnetz
 
 # Putting the features together
 def get_feature(file_path):
