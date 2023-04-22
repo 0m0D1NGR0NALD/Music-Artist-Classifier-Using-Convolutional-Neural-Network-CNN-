1fd737ce199c9e7762b43d4b0e50a6f8585cbf90
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-22 08:46:36 +0300

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -15,14 +15,14 @@ def get_feature(file_path):
   melspectrogram_mean = melspectrogram.mean(axis=1)
   melspectrogram_min = melspectrogram.min(axis=1)
   melspectrogram_max = melspectrogram.max(axis=1)
-  melspectrogram_feature = numpy.concatenate( (melspectrogram_mean, melspectrogram_min, melspectrogram_max) )
+  melspectrogram_feature = numpy.concatenate((melspectrogram_mean, melspectrogram_min, melspectrogram_max))
 
   # Extracting chroma vector feature
   chroma = get_chroma_vector(file_path)
   chroma_mean = chroma.mean(axis=1)
   chroma_min = chroma.min(axis=1)
   chroma_max = chroma.max(axis=1)
-  chroma_feature = numpy.concatenate( (chroma_mean, chroma_min, chroma_max) )
+  chroma_feature = numpy.concatenate((chroma_mean, chroma_min, chroma_max))
   
   # Extracting tonnetz feature
   tntz = get_tonnetz(file_path)
