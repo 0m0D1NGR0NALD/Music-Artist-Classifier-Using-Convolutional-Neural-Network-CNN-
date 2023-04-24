dc98094a8b50b72d7c00d1064ecc6061aa4aab65
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-24 15:05:13 +0300

    Update features.py

--- features.py
+++ features.py
@@ -1,3 +1,6 @@
 import os
 import numpy
 from features_extractor import get_feature
+
+# Calculating features for the dataset
+directory = 'path to dataset' # Dataset contains a folder for each class: 'chameleon','juliana','kenzo','rema'
