b79460312906e7856828f990d738a082fdcb523c
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-24 15:09:36 +0300

    Update features.py

--- features.py
+++ features.py
@@ -5,6 +5,7 @@ from features_extractor import get_feature
 # Calculating features for the dataset
 directory = 'path to dataset' # Dataset contains a folder for each class: 'chameleon','juliana','kenzo','rema'
 artists = ['chameleon','juliana','kenzo','rema']
+
 features = []
 labels = []
 
