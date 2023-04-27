615fb614034d2e5c77255c55dd22a739e0ceefb6
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-27 15:47:36 +0300

    Update visualize_results.py

--- visualize_results.py
+++ visualize_results.py
@@ -1,2 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from features import model
+
+# Storing model history of performance within variable 'metrics'
+metrics = pd.DataFrame(model.history.history)
