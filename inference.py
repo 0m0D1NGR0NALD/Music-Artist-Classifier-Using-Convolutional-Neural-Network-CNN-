188ab4e3b69645d51de1f0e1e908e1cd0bcbfb7c
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-29 10:37:20 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,3 +3,7 @@ from pydub import AudioSegment
 import IPython.display as ipd
 from features import model
 from features_extractor import get_feature
+
+# Crop wavefile to a 30 secods section and save the resulting file
+t1 = 30000 # In milliseconds
+t2 = 60000
