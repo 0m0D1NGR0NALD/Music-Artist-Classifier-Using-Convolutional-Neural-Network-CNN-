26e86729db812c0745761069f05a459103f49089
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-29 10:39:08 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -6,6 +6,7 @@ from features_extractor import get_feature
 
 # Path to inference data samples
 chameleon = 'Badilisha.mp3'
+rema = 'Lowooza Kunze.mp3'
 
 # Crop wavefile to a 30 secods section and save the resulting file
 t1 = 30000 # In milliseconds
