df4765e36575c1d5fffcd39c15d7ae6998baec80
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-29 10:38:47 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,6 +4,9 @@ import IPython.display as ipd
 from features import model
 from features_extractor import get_feature
 
+# Path to inference data samples
+chameleon = 'Badilisha.mp3'
+
 # Crop wavefile to a 30 secods section and save the resulting file
 t1 = 30000 # In milliseconds
 t2 = 60000
