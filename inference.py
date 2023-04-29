e35372c929f7ae9f9eaf42f3bab286b2fe14aaef
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-04-29 10:40:45 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -10,6 +10,9 @@ rema = 'Lowooza Kunze.mp3'
 juliana = 'Sanyu Lyange.mp3'
 kenzo = 'Sitya Loss.mp3'
 
+# Selecting sample
+song = chameleon
+
 # Crop wavefile to a 30 secods section and save the resulting file
 t1 = 30000 # In milliseconds
 t2 = 60000
